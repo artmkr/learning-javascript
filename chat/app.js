d0a45e7e5196069b8f0e5f26c088b4e2bb7f2df8
nuenvio <nuenvio@gmail.com>
2016-03-02 18:44:44 +0300

    writed simple chat app

--- chat/app.js
+++ chat/app.js
@@ -1,20 +1,28 @@
 var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
 var app = express();
+
+var path = require('path');
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
-app.listen(3000, function() {
+io.on('connection', function(socket) {
+    socket.on('message', function(msg) {
+        io.emit('message', msg);
+    });
+});
+
+
+http.listen(3000, function() {
     console.log("express has started on port 3000");
 });
 
---
+++ chat/public/javascripts/script.js
@@ -0,0 +1,9 @@
+var socket = io();
+$('form').submit(function() {
+    socket.emit('message', $('#m').val());
+    $('#m').val('');
+    return false;
+});
+socket.on('message', function(msg) {
+    $('#messages').append($('<li>').text(msg));
+});
