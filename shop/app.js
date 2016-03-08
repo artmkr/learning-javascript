289c05e16c1b06160e3a2a8d70d006abbfa2cbbc
nuenvio <nuenvio@gmail.com>
2016-03-09 00:20:53 +0300

    created post and get requestes

--- shop/app.js
+++ shop/app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/shop');
 
-var Item = mongoose.model('Item', { name: String,cost: Number });
+var Item = mongoose.model('Item', { name: String,price: Number });
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +16,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
     res.render('index');
 });
+
+app.get('/shop', function(req, res){
+  Item.find({}, function(err, items) {
+    var itemMap = [];
+    items.forEach(function(item) {
+      itemMap.push({name:item.name, price: item.price});
+    });
+    res.send(itemMap);
+  });
+});
+
+
+app.post('/shop', function(req, res){
+  var newItem = Item({name:"Hello" , price:3});
+  newItem.save(function(err) {
+  if (err) throw err;
+    console.log('User created!');
+  });
+  res.status(201).send();
+});
+
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
