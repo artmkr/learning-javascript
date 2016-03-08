d6721444a57ad4a87715669fc3854cea85808a8f
nuenvio <nuenvio@gmail.com>
2016-03-08 23:56:41 +0300

    added mongoose model for store item

--- shop/app.js
+++ shop/app.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/shop');
+
+var Item = mongoose.model('Item', { name: String,cost: Number });
+
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
 
